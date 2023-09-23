0370412303d998c3aebd80b89a522a97737ea2fe
DavidWu-agk <145793123+DavidWu-agk@users.noreply.github.com>
2023-09-23 22:00:05 +0800

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        int n = input.nextInt();
+        int i = m ;
+        int sum=0;
+        while(i>=m & i<=n){
+            int k = 0;
+            for(int j=1;j<=i;j++){
+                if(i%j==0){
+                    k++;
+                }
+            }
+            if(k==4){
+                sum = sum + i;
+            }
+            i=i+1;
+        }
+        System.out.print(sum);
+    }
+}
